fe6d281f3d4464f769c422d563e1a76fd1c129c7
LYH2263 <lyh2263@users.noreply.github.com>
2026-08-17 23:55:38 +0800

    修复：前缀扫描边界

--- internal/prefixscan/prefixscan.go
+++ internal/prefixscan/prefixscan.go
@@ -3,6 +3,6 @@ type Iter struct { keys []string; i int }
 func New(keys []string) *Iter { return &Iter{keys: keys} }
 func (it *Iter) Seek(prefix string) {
 	it.i = 0
-	for it.i < len(it.keys) && it.keys[it.i] <= prefix { it.i++ }
+	for it.i < len(it.keys) && it.keys[it.i] < prefix { it.i++ }
 }
 func (it *Iter) Key() string { if it.i >= len(it.keys) { return "" }; return it.keys[it.i] }
